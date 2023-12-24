2f30c7f5a36f89bb90355ed1287fd532e2b52357
AFSAL333 <afsalafs8008@gmail.com>
2023-12-25 01:45:24 +0530

    start placeholder

--- client/src/assets/images/imageLinks.js
+++ client/src/assets/images/imageLinks.js
@@ -2,6 +2,7 @@ const imageLinks = {
     auth: 'https://res.cloudinary.com/cloudverse/image/upload/v1697283309/CHATHUB/ydy474ufdevqdtv0yoqp.jpg',
     logo: 'https://res.cloudinary.com/cloudverse/image/upload/v1695133216/CODEVERSE/logo.png',
     profile: 'https://res.cloudinary.com/cloudverse/image/upload/v1695133216/CODEVERSE/profile.jpg',
+    horizontal: 'https://res.cloudinary.com/cloudverse/image/upload/v1703225521/CODEVERSE/horizontal.png',
   };
   
   export default imageLinks;
--- client/src/components/auth/AuthCard.jsx
+++ client/src/components/auth/AuthCard.jsx
@@ -1,13 +1,25 @@
 import PropTypes from "prop-types";
+import { Image as AntImage } from "antd";
 import imageLinks from "../../assets/images/imageLinks";
 
 function AuthCard({ children }) {
   return (
     <section className="min-h-screen flex items-center justify-center">
-      <div className="flex rounded-2xl shadow-2xl max-w-4xl bg-light-dark  p-5 m-1 items-center">
+      <div className="flex rounded-2xl shadow-2xl max-w-4xl bg-light-dark p-5 m-1 items-center">
         <div className="md:w-1/2 px-8 md:px-14">{children}</div>
-        <div className="md:block hidden w-1/2">
-          <img className="rounded-2xl" src={imageLinks.auth} alt="Auth" />
+        <div className="md:flex hidden w-1/2">
+          {imageLinks.auth ? (
+            <img className="rounded-2xl" src={imageLinks.auth} alt="Auth" />
+          ) : (
+            <div className="flex items-center rounded-2xl justify-center bg-gray-300 animate-pulse w-[482px] h-[573px]">
+              <AntImage
+                preview={false}
+                src={imageLinks.horizontal}
+                alt="Image Skeleton"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </section>
