49ff3295319d2acd36fa8f693b4123b18159f476
AFSAL333 <afsalafs8008@gmail.com>
2024-08-03 13:39:25 +0530

    logo updated

--- client/src/assets/images/imageLinks.js
+++ client/src/assets/images/imageLinks.js
@@ -1,6 +1,6 @@
 const imageLinks = {
   auth: 'https://res.cloudinary.com/cloudverse/image/upload/v1697283309/CHATHUB/ydy474ufdevqdtv0yoqp.jpg',
-  logo: 'https://res.cloudinary.com/cloudverse/image/upload/v1695133216/CODEVERSE/logo.png',
+  logo: 'https://res.cloudinary.com/cloudverse/image/upload/v1721893242/CODSIFY/codsify.png',
   profile: 'https://res.cloudinary.com/cloudverse/image/upload/v1695133216/CODEVERSE/profile.jpg',
   horizontal: 'https://res.cloudinary.com/cloudverse/image/upload/v1703225521/CODEVERSE/horizontal.png',
 };
